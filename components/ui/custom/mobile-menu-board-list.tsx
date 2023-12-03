1d2fb259b10426ac6284cefde572b2221cc29d02
Talha Imran <talhaimran284@gmail.com>
2023-12-04 00:04:50 +0500

    Added a key prop

--- components/ui/custom/mobile-menu-board-list.tsx
+++ components/ui/custom/mobile-menu-board-list.tsx
@@ -4,7 +4,7 @@ import { Board } from 'types/mock'
 import BoardMenuItem from 'custom/board-menu-item'
 
 const MobileMenuBoardList = ({ boards }: { boards: Board[] }) => {
-  return boards.map((board) => <BoardMenuItem board={board} />)
+  return boards.map((board) => <BoardMenuItem key={board.name} board={board} />)
 }
 
 export default MobileMenuBoardList

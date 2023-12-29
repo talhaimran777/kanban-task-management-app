ab020f80816f37932d9db72a8b396a48274fe46d
Talha Imran <talhaimran284@gmail.com>
2023-12-29 19:28:23 +0500

    fix: added a state to fix the initial toggling issue based on the theme value.

--- components/ui/custom/theme-toggler.tsx
+++ components/ui/custom/theme-toggler.tsx
@@ -5,20 +5,22 @@ import LightThemeIcon from 'assets/svg-icons/LightThemeIcon'
 import { Switch } from 'ui/switch'
 
 import { useTheme } from 'next-themes'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 const ThemeToggler = () => {
   const { theme, setTheme } = useTheme()
+  const [currentTheme, setCurrentTheme] = useState("");
 
   useEffect(() => {
     window.localStorage.setItem('theme', theme as string)
+    setCurrentTheme(theme as string);
   }, [theme])
 
   return (
     <div className='flex justify-center items-center bg-grey-secondary py-[14px] mx-6 gap-6 rounded-md my-4 dark:bg-very-dark-grey'>
       <LightThemeIcon className='fill-grey-ternary' />
       <Switch
-        checked={theme === 'dark'}
+        checked={currentTheme === "dark"}
         onCheckedChange={(checked) => setTheme(checked ? 'dark' : 'light')}
       />
       <DarkThemeIcon className='fill-grey-ternary' />

a238ae33e41e5e56db39eec3848b8d8891b0f48a
Talha Imran <talhaimran284@gmail.com>
2024-06-29 17:02:05 +0500

    refactor: delete unused file

--- src/components/layout/contianer.tsx
+++
@@ -1,9 +0,0 @@
-const Container = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <div className='text-black-primary dark:text-white px-6'>
-            {children}
-        </div>
-    )
-}
-
-export default Container

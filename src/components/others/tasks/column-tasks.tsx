ba7ac6230ec1799793e585b9fb53992393a16708
Talha Imran <talhaimran284@gmail.com>
2024-12-07 17:15:12 +0500

    add scroll to tasks list

--- src/components/others/tasks/column-tasks.tsx
+++ src/components/others/tasks/column-tasks.tsx
@@ -2,7 +2,13 @@ import ColumnTask from 'src/components/others/tasks/column-task'
 import { Task } from 'src/types/mock'
 
 const ColumnTasks = ({ tasks }: { tasks: Task[] }) => {
-    return tasks.map((task, index) => <ColumnTask key={task.id} task={task} />)
+    return (
+        <div className='overflow-auto flex flex-col gap-6'>
+            {tasks.map((task) => (
+                <ColumnTask key={task.id} task={task} />
+            ))}
+        </div>
+    )
 }
 
 export default ColumnTasks
